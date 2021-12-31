fd0c7012b5b26565b9d832c2fcb6f278f555d268
Shannen Geller <45742566+shannengeller@users.noreply.github.com>
2021-12-31 13:37:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -8,40 +8,41 @@ let status = "stopped";
 
 //function that updates the watch with the accurate seconds and minutes
 function watch(){
+    
     seconds++;
 
     if(seconds/60 === 1){
         seconds = 0;
         minutes++;
     }
-    
     if (seconds < 10){
         displaySeconds = "0" + seconds.toString();
     }
     else{
         displaySeconds = seconds;
     }
-
     if(minutes < 10){
         displayMinutes = "0" + minutes.toString();
     }
     else{
         displayMinutes = minutes;
     }
+    if(minutes >= 60){
+        window.clearInterval(interval);
+        document.getElementById("display").innerHTML = "00:00";
+    }
     
     document.getElementById("display").innerHTML = displayMinutes + ":" + displaySeconds;
-
 }
 
-//playpauses homie no cap no kiz
 function playPause() {
+
     if (status === "stopped"){
 
         //starts the stopwatch
         interval = window.setInterval(watch, 1000);
         document.getElementById("playPause").innerHTML="Stop";
         status = "started";
-
     }
     else{
         window.clearInterval(interval);
@@ -59,6 +60,17 @@ function reset(){
     document.getElementById("playPause").innerHTML = "Start";
 }
 
-// function addTenMins(){
-//     
-// }
+ function addTenMins(){
+
+     if(minutes < 10){
+         minutes = 10;
+         document.getElementById("display").innerHTML = "10:00";
+     }
+    else{
+        minutes = minutes + 10;
+        document.getElementById("display").innerHTML = "10:00" + minutes;
+        if(minutes >= 60){
+            document.getElementById("display").innerHTML = "00:00";
+        }
+    }
+ }
